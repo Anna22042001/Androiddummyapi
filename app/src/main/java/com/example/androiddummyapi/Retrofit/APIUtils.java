440d00b40d7aa4e6529f2af4891f8caac657290a
Anna22042001 <60919541+Anna22042001@users.noreply.github.com>
2020-06-02 20:17:44 +0700

    Update APIUtils.java

--- app/src/main/java/com/example/androiddummyapi/Retrofit/APIUtils.java
+++ app/src/main/java/com/example/androiddummyapi/Retrofit/APIUtils.java
@@ -2,7 +2,7 @@ package com.example.androiddummyapi.Retrofit;
 
 
 public class APIUtils {
-    public static final String Base_url = "https://my-json-server.typicode.com/Anna22042001/androidinternship/db/";
+    public static final String Base_url = "https://my-json-server.typicode.com/Anna22042001/androidinternship/";
     public static Dataservice getDataservice(){
 
         return RetrofitClient.getClient(Base_url).create(Dataservice.class);
